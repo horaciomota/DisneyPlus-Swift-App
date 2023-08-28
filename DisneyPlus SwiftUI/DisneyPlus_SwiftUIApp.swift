96a0cf8e840decb3eb3156f2edff04e699120f1b
Horacio Mota <horaciomota93@gmail.com>
2023-08-28 20:53:38 +0100

    Initial Commit

---
+++ DisneyPlus SwiftUI/DisneyPlus_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlus_SwiftUIApp.swift
+//  DisneyPlus SwiftUI
+//
+//  Created by Horacio Mota on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlus_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
